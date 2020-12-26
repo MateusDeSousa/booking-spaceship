794b630f12874ce93570afdce45b5ae9646e2308
Mateus Sousa <mateusdevsousa@gmail.com>
2020-12-26 10:39:26 -0300

    Start project

---
+++ booking-spaceship WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  booking-spaceship WatchKit Extension
+//
+//  Created by Mateus Sousa on 26/12/20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ booking-spaceship WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  booking-spaceship WatchKit Extension
+//
+//  Created by Mateus Sousa on 26/12/20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
